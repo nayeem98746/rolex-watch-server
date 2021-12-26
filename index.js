4c7c8fb97c0fcd6b0bd489e6545525084c118c92
nayeem407 <sabbirahmednayeem941@gmail.com>
2021-12-26 12:09:53 +0600

    second commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require ('express')
+const { MongoClient } = require('mongodb');
+const cors = require('cors')
+require('dotenv').config()
+
+
+const app = express()
+const port = 5000;
+
+// middleware 
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t8ils.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try{
+        await client.connect();
+        const database = client.db('rolexWatch')
+        const servicesCollection = database.collection('service');
+
+        // POST API
+        app.post('/services', async(req , res) => {
+            const service = req.body
+            console.log('hit the api ' , service)
+            const result = await servicesCollection.insertOne(service)
+            console.log(result)
+            res.json(result)
+        })
+        // Get api
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({})
+            const service =  await cursor.toArray()
+            res.send(service)
+        })
+
+
+
+    }
+    finally{
+        // await client.close()
+    }
+}
+run().catch(console.dir);
+
+app.get('/' , (req, res) => {
+    res.send('Running Genius Server')
+} )
+
+app.listen(port , () => {
+    console.log('Running Genius Server on Port', port)
+})
